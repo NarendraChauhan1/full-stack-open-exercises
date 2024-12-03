5c6589157b6f8f05e5ec995b02796987ef0aa761
Narendra Chauhan <karanchavhan58@gmail.com>
2024-12-04 00:13:39 +0530

    Completed exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,20 +1,30 @@
 import { useState } from 'react'
 
-const ParaText = ({text}) => <p>{text}</p>
-
 const TitleText = ({text}) => <h1>{text}</h1>
 
 const Sum = (newValue) => newValue((previousValue) => previousValue + 1)
 
 const Button = ({eventHandler, text}) => <button onClick={eventHandler}>{text}</button>
 
+const Statistics = ({good, neutral, bad}) => {
+  const Total = good + neutral + bad
+  return (
+    <div>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {Total}</p>
+    <p>average {((good * 1) + (neutral * 0) + (bad * -1))/Total}</p>
+    <p>positive {(good/Total) * 100} %</p>
+    </div>
+  )
+}
+
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const Total = good + neutral + bad
 
   return (
     <div>
@@ -23,12 +33,7 @@ const App = () => {
     <Button eventHandler={() => Sum(setNeutral)} text={"neutral"}/>
     <Button eventHandler={() => Sum(setBad)} text={"bad"}/>
     <TitleText text={"statistics"} />
-    <ParaText text={"good" + " " + good} />
-    <ParaText text={"neutral" + " " + neutral} />
-    <ParaText text={"bad"+ " " + bad} />
-    <ParaText text={"all" + " " + Total} />
-    <ParaText text={"average" + " " + ((good * 1) + (neutral * 0) + (bad * -1))/Total} />
-    <ParaText text={"positive" + " " + (good/Total) * 100 + " " + "%"} />
+    <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
