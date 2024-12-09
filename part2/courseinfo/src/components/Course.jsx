f41070c7d3b947d4960516a18f16c803bc3b5a17
Narendra Chauhan <karanchavhan58@gmail.com>
2024-12-10 00:14:15 +0530

    Completed exercise 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,15 +1,19 @@
 import Header from './Header'
 import Content from './Content'
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
     return (
-        <div>
-            <Header header={course[0].name}/>
-            <Content content={course[0].parts}/>
-            <Header header={course[1].name}/>
-            <Content content={course[1].parts}/>
-        </div>
-    )
-}
+      <div>
+        {
+          course.map(courses => 
+            <div key={courses.id}>
+              <Header header={courses.name} />
+              <Content content={courses.parts} />
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 
 export default Course
