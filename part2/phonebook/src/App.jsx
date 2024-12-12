ef16e9795934ce7a61c8c527834032de338541bf
Narendra Chauhan <karanchavhan58@gmail.com>
2024-12-12 13:21:40 +0530

    Completed exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,8 +3,10 @@ import Name from './components/Name'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'
-     }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
@@ -16,7 +18,8 @@ const App = () => {
     event.preventDefault()
     const newPersons = {
       name: newName,
-      number: newPhone
+      number: newPhone,
+      id: String(persons.length + 1)
     }
 
     persons.some(person => person.name === newName ) ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(newPersons))
@@ -24,9 +27,20 @@ const App = () => {
     setNewName('')
   } 
 
+  const filter = (event) => {
+    const filterName = event.target.value
+    const filerList = [persons.find(person => person.name === filterName)]
+    const copyPersons = persons
+    persons.some(person => person.name.toLowerCase() === filterName.toLowerCase()) ? setPersons(filerList) : setPersons(copyPersons)    
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with <input onChange={filter}/>
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={saveNameFunc}>
         <div>
           name: <input value={newName} onChange={newNameFunc} placeholder='Enter your name' required/>
@@ -40,7 +54,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map(contact => <Name names={contact} key={contact.name}/>)}
+      {persons.map(contact => <Name names={contact} key={contact.id}/>)}
       </ul>
     </div>
   )
